d0ab3d4be043a7fafe21e56c4d47f398d8b4b983
Santiago <santiagobejaranoariza@gmail.com>
2024-11-20 13:19:56 -0500

    Primera interfaz Click Munch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = ({ cartCount }) => {
   return (
     <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', background: '#4caf50', color: 'white' }}>
-      <h1>FoodMart</h1>
+      <h1>ClickMunch</h1>
       <div>🛒 Cart: {cartCount}</div>
     </nav>
   );
